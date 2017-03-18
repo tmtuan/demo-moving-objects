a3e35e866dfb68b5f64daa16441698eb52cbf221
mtuan <tranminhtuan684@gmail.com>
2017-03-18 11:30:24 +0700

    Initial demo-moving-objects

---
+++ moving-objects/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  moving-objects
+//
+//  Created by ThaoLT on 3/18/17.
+//  Copyright © 2017 tmtuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ moving-objects/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  moving-objects
+//
+//  Created by ThaoLT on 3/18/17.
+//  Copyright © 2017 tmtuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
